9d8c012f17938c57caa4008a3664b3dcae9e13d5
wshkaah <sasanmalikov@gmail.com>
2026-07-21 01:04:06 +0900

    Done sumDounle

---
+++ src/main/java/org/example/WarmUp1/SumDouble.java
@@ -0,0 +1,11 @@
+package org.example.WarmUp1;
+
+public class SumDouble {
+    public int sumDouble(int a, int b) {
+        if(a == b){
+            return (a + b)*2;
+        }
+        return a + b;
+    }
+
+}
